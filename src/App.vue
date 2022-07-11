b7ba91d9a8588b5b58cafa0eb47ff1c48d5e5407
manish jadli <manish.jadli@irasys.biz>
2022-07-11 08:56:39 +0530

    add bootstrap  code commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-<router-link to="/" >Home</router-link>
-<router-link to="/about" >About</router-link>
-<router-link to="/contact" >Contact</router-link>
+
+<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+  <div class="container-fluid">
+    <router-link to="/" class="navbar-brand">Logo</router-link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="mynavbar">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
 <div class="container">
     <router-view></router-view>
