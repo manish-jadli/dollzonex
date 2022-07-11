c130eef5a376c1f01ce849066cadd205009f0085
manish jadli <manish.jadli@irasys.biz>
2022-07-11 18:05:56 +0530

    add login, signup & logout

---
+++ src/components/LogOut.vue
@@ -0,0 +1,16 @@
+<template>
+<h2>Logout successfully.</h2>
+<p>If you want to again login then please click this <router-link to="/login">link</router-link></p>
+
+</template>
+
+
+<script>
+export default {
+  name: 'LogOut',
+  props: {
+    homemsg: String,
+    homeheaderParagraph:String
+  }
+}
+</script>
--- src/routes.js
+++ src/routes.js
@@ -3,16 +3,22 @@ import { createWebHashHistory, createRouter, createWebHistory } from "vue-router
 import Home from './components/Home.vue'
 import About from './components/About.vue'
 import Contact from './components/Contact.vue'
+import Login from './components/Login.vue'
+import Signup from './components/Signup.vue'
+import LogOut from './components/LogOut.vue'
 
 
-const routes=[
-    {name:'Home', path:'/',component:Home},
-    {name:'About', path:'/about',component:About},
-    {name:'Contact', path:'/contact',component:Contact}
+const routes = [
+    { name: 'Login', path: '/login', component: Login },
+    { name: 'Signup', path: '/signup', component: Signup },
+    { name: 'LogOut', path: '/logout', component: LogOut },
+    { name: 'Home', path: '/', component: Home },
+    { name: 'About', path: '/about', component: About },
+    { name: 'Contact', path: '/contact', component: Contact }
 ];
 
-const router=createRouter({
-    history:createWebHistory(),
+const router = createRouter({
+    history: createWebHistory(),
     routes
 });
 
